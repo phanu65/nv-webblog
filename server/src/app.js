57dc36931b34f99f900d5ae677e02b657db1474f
WHmild04 <gmpwh04@gmail.com>
2024-07-09 09:42:13 +0700

    'server'

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+let express = require('express');
+const app = express();
+
+app.get('/status', function(req, res) {
+    res.send('Hello nodejs server')
+})
+
+let port = 9900
+
+
+app.listen(port, function() {
+    console.log('server running on ' + port)
+})
