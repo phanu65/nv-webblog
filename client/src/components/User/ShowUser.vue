409fc8b441b7468ea910b29095dce9f5767d4fb6
WHmild04 <gmpwh04@gmail.com>
2024-08-09 10:02:14 +0700

    server

---
+++ client/src/components/User/ShowUser.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Get Users By id</h1>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped></style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import UserIndex from '@/components/User/Index'
+import UserCreate from '@/components/User/CreateUser'
+import UserEdit from '@/components/User/EditUser'
+import UserShow from '@/components/User/ShowUser'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        path: '/user',
+        name: 'user',
+        component: UserIndex
+    }, {
+        path: '/user/create',
+        name: 'users-create',
+        component: UserCreate
+    }, {
+        path: '/user/edit',
+        name: 'user-edit',
+        component: UserEdit
+    }, {
+        path: '/users',
+        name: 'users',
+        component: UserShow
+    }, ]
 })
